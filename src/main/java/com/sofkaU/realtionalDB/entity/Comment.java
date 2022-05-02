2e807326f23811c093bec5edad7dbfa01633e313
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-05-01 22:30:07 -0500

    Add Entity
    Creation of Entity package and its classes post and comments

--- src/main/java/com/sofkaU/realtionalDB/entity/Comment.java
+++ src/main/java/com/sofkaU/realtionalDB/entity/Comment.java
@@ -1,4 +1,18 @@
 package com.sofkaU.realtionalDB.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity(name = "Commet")
+@Table(name = "comment")
+@Data
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private long id;
+    private String message;
+    private Long FK_post_id;
+
 }
